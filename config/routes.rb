0dc7ac35d9f4eb38523869aa5d1233cd45ba5e55
aaditya91 <aaditya@freshdesk.com>
2013-09-14 16:13:06 +0530

    Added extra devise stuff:

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Work::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +40,7 @@ Work::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
